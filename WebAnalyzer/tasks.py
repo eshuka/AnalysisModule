4644c14934bae9ff1eae5cdee638efe54d7ccace
RockSakong <fax369@hanmail.net>
2018-04-25 02:57:45 +0000

    Changes to be committed: 	modified:   WebAnalyzer/tasks.py

--- WebAnalyzer/tasks.py
+++ WebAnalyzer/tasks.py
@@ -25,8 +25,8 @@ def module_load_init(**__):
     #   - Add your model
     #   - You can use worker_index if you need to get and set gpu_id
     #       - ex) gpu_id = worker_index % TOTAL_GPU_NUMBER
-    from Modules.dummy.main import Dummy
-    analyzer = Dummy()
+    from Modules.places.main import Places
+    analyzer = Places()
 
 
 @app.task
